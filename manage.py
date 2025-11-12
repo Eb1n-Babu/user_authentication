bde99be8a2575fb56e6cf6ed6ce42bfd6316a050
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-11-12 17:49:50 +0530

    peoject updated

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'x1.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registration-login.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- registration-login/urls.py
+++ registration-login/urls.py
@@ -19,5 +19,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('y1.urls')),
+    path('',include('registration_login_app.urls')),
 ]
