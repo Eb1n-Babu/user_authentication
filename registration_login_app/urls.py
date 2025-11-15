b5d5f13566c6d982c546317a0f7162baeaba642c
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-11-15 11:48:31 +0530

    project updated

--- registration_login_app/urls.py
+++ registration_login_app/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 
     path('login/user/', RegisterView.as_view({'get': 'list', 'post': 'create'}),name='user'),
     path('login/user/<int:pk>/', RegisterView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('login/user/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/user/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
